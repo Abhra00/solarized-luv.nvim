8fe3c35b8b54e3cc0c58d5ed54ce821875a126fe
Bugs <abhramondal9674@gmail.com>
2026-01-10 16:29:53 +0530

    Fix syntax by adding commas to color definitions

--- lua/solarized-luv/groups/telescope.lua
+++ lua/solarized-luv/groups/telescope.lua
@@ -6,10 +6,10 @@ function M.get(c)
   return {
     TelescopeBorder               = { fg = c.border, bg = c.bg_float },
     TelescopeNormal               = { fg = c.fg, bg = c.bg_float },
-    TelescopeResultsDiffAdd       = { fg = c.green03 }
-    TelescopeResultsDiffChange    = { fg = c.yellow03 }
-    TelescopeResultsDiffDelete    = { fg = c.red03 }
-    TelescopeResultsDiffUntracked = { fg = c.orange03 }
+    TelescopeResultsDiffAdd       = { fg = c.green03 },
+    TelescopeResultsDiffChange    = { fg = c.yellow03 },
+    TelescopeResultsDiffDelete    = { fg = c.red03 },
+    TelescopeResultsDiffUntracked = { fg = c.orange03 },
   }
 end
 
